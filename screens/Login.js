2369932a571679353b4e0993653ade9ea46ecc4f
OctoOcto <66621960+OctoOcto@users.noreply.github.com>
2020-08-11 14:38:01 +0200

    login page

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { StyleSheet, Text, View, AsyncStorage, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  AsyncStorage,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import {
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 const Login = ({ setToken }) => {
+  const navigation = useNavigation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -27,13 +39,17 @@ const Login = ({ setToken }) => {
     }
   };
   return (
-    <SafeAreaView>
-      <View>
-        <Image source={require("../assets/Vector.png")}></Image>
+    <SafeAreaView style={{ backgroundColor: "#F35960", flex: 1 }}>
+      <View style={styles.header}>
+        <Image
+          style={{ width: 100 }}
+          source={require("../assets/Vector.png")}
+        ></Image>
       </View>
-      <View>
+      <View style={{ alignItems: "center" }}>
         <TextInput
           autoCapitalize="none"
+          style={styles.input}
           value={email}
           onChangeText={(text) => {
             setEmail(text);
@@ -41,14 +57,25 @@ const Login = ({ setToken }) => {
         />
         <TextInput
           secureTextEntry={true}
+          style={styles.input}
           value={password}
           onChangeText={(text) => {
             setPassword(text);
           }}
         />
-        <TouchableOpacity onPress={handleSubmit}>
-          <Text>Se connecter</Text>
+        <TouchableOpacity style={styles.submit} onPress={handleSubmit}>
+          <Text style={styles.textSubmit}>Se connecter</Text>
         </TouchableOpacity>
+      </View>
+
+      <View>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            navigation.navigate("Signup");
+          }}
+        >
+          <Text style={styles.signupButton}>Pas de compte ? S'inscrire</Text>
+        </TouchableWithoutFeedback>
       </View>
     </SafeAreaView>
   );
@@ -56,4 +83,39 @@ const Login = ({ setToken }) => {
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 109,
+    marginBottom: 129,
+    width: "100%",
+    alignItems: "center",
+  },
+  input: {
+    marginBottom: 59,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    borderColor: "white",
+    width: 320,
+    borderBottomColor: "white",
+    color: "white",
+    fontSize: 16,
+    lineHeight: 18,
+  },
+  submit: {
+    width: 190,
+    height: 65,
+    backgroundColor: "white",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textSubmit: {
+    fontSize: 24,
+    color: "#F35960",
+  },
+  signupButton: {
+    color: "white",
+    textAlign: "center",
+    marginTop: 43,
+  },
+});
